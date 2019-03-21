70d3255bb37a3ce5618489da32936431d9c9fa97
Žiga Vene <zv3896@student.uni-lj.si>
2019-03-22 00:44:15 +0100

    Abstr: Basic type support.

--- prev/srcs/compiler/phases/abstr/AbsTreeConstructor.java
+++ prev/srcs/compiler/phases/abstr/AbsTreeConstructor.java
@@ -7,7 +7,9 @@ import java.util.*;
 import compiler.common.report.*;
 import compiler.data.dertree.*;
 import compiler.data.dertree.visitor.*;
+import compiler.data.symbol.Symbol;
 import compiler.data.abstree.*;
+import compiler.data.abstree.AbsAtomType.Type;
 
 /**
  * Transforms a derivation tree to an abstract syntax tree.
@@ -51,7 +53,34 @@ public class AbsTreeConstructor implements DerVisitor<AbsTree, AbsTree> {
 				allDecls.addAll(decls.decls());
 			return new AbsDecls(new Location(decl, decls == null ? decl : decls), allDecls);
 		}
-
-		// TODO
-
+		
+		case Decl: {
+			DerLeaf nodeType = ((DerLeaf)node.subtree(0));
+			switch (nodeType.symb.token) {
+			case TYP:
+				AbsType type = (AbsType) node.subtree(3).accept(this, null);
+				String name = ((DerLeaf)(node.subtree(1))).symb.lexeme;
+				return new AbsTypDecl(new Location(type, type), name, type);
+			case VAR:
+				//TODO
+				break;
+			case FUN:
+				//TODO
+				break;
+			default:
+				System.out.println("Something went wrong");
+			}
+		}
+		
+		case Type:{
+			return new AbsAtomType(new Location(node, node), Type.INT);
+		}
+		
+		case Stmts:{
+			Vector<AbsStmt> allStmts = new Vector<AbsStmt>();
+		}
+		default: System.out.println(node.label);
+		}
+		return null;
+	}
 }
