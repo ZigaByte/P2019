e571920dc27c3e9fae09e239c5d26147e0f3c661
Žiga Vene <zv3896@student.uni-lj.si>
2019-03-22 00:43:21 +0100

    Updated bat for Abstr

--- xsltproc/generate_xml.bat
+++ xsltproc/generate_xml.bat
@@ -1,2 +1,3 @@
 xsltproc.exe --output output/lexan.html ../prev/data/lexan.xsl ../tests/test.lexan.xml
 xsltproc.exe --output output/synan.html ../prev/data/synan.xsl ../tests/test.synan.xml
+xsltproc.exe --output output/abstr.html ../prev/data/abstr.xsl ../tests/test.abstr.xml
