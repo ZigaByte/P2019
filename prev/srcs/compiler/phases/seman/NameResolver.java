ab690528cbe6304e04a42aa6a83c8e7884c47e93
Žiga Vene <zv3896@student.uni-lj.si>
2019-03-30 00:07:39 +0100

    Seman: Name resolving test.

--- prev/srcs/compiler/phases/seman/NameResolver.java
+++ prev/srcs/compiler/phases/seman/NameResolver.java
@@ -4,8 +4,11 @@
 package compiler.phases.seman;
 
 import compiler.common.report.*;
+import compiler.common.report.Report.Error;
 import compiler.data.abstree.*;
 import compiler.data.abstree.visitor.*;
+import compiler.phases.seman.SymbTable.CannotFndNameException;
+import compiler.phases.seman.SymbTable.CannotInsNameException;
 
 /**
  * Name resolving: the result is stored in {@link SemAn#declaredAt}.
@@ -16,7 +19,71 @@ public class NameResolver extends AbsFullVisitor<Object, Object> {
 
 	/** Symbol table. */
 	private final SymbTable symbTable = new SymbTable();
+	
+	@Override
+	public Object visit(AbsTypDecl typDecl, Object visArg) {
+		try {
+			symbTable.ins(typDecl.name, typDecl);
+		} catch(CannotInsNameException e) {
+			throw new Report.Error("[nameResolution] " + typDecl.location() + " Could not insert " + typDecl.name);
+		}
+		return super.visit(typDecl, visArg);
+	}
+	
+	@Override
+	public Object visit(AbsFunDef funDef, Object visArg) {
+		try {
+			symbTable.ins(funDef.name, funDef);
+		} catch(CannotInsNameException e) {
+			throw new Report.Error("[nameResolution] " + funDef.location() + " Could not insert " + funDef.name);
+		}
+		return super.visit(funDef, visArg);
+	}
+	
+	@Override
+	public Object visit(AbsFunDecl funDecl, Object visArg) {
+		try {
+			symbTable.ins(funDecl.name, funDecl);
+		} catch(CannotInsNameException e) {
+			throw new Report.Error("[nameResolution] " + funDecl.location() + " Could not insert " + funDecl.name);
+		}
+		return super.visit(funDecl, visArg);
+	}
+	
+	@Override
+	public Object visit(AbsVarDecl varDecl, Object visArg) {
+		try {
+			symbTable.ins(varDecl.name, varDecl);
+		} catch(CannotInsNameException e) {
+			throw new Report.Error("[nameResolution] " + varDecl.location() + " Could not insert " + varDecl.name);
+		}
+		return super.visit(varDecl, visArg);
+	}
+	
+	@Override
+	public Object visit(AbsParDecl parDecl, Object visArg) {
+		//symbTable.newScope();
+		// TODO Auto-generated method stub
+		return super.visit(parDecl, visArg);
+	}
+	
+	@Override
+	public Object visit(AbsFunName funName, Object visArg) {
+		// TODO Auto-generated method stub
+		return super.visit(funName, visArg);
+	}
+	
 
-	// TODO
+	
+	@Override
+	public Object visit(AbsTypName varDecl, Object visArg) {
+		System.out.println("hell2o");
+		try {
+			SemAn.declaredAt.put(varDecl, symbTable.fnd(varDecl.name));
+		} catch(CannotFndNameException e) {
+			throw new Report.Error("Test name ");
+		}
+		return super.visit(varDecl, visArg);
+	}
 
 }
--- xsltproc/generate_xml.bat
+++ xsltproc/generate_xml.bat
@@ -1,3 +1,4 @@
 xsltproc.exe --output output/lexan.html ../prev/data/lexan.xsl ../tests/test.lexan.xml
 xsltproc.exe --output output/synan.html ../prev/data/synan.xsl ../tests/test.synan.xml
 xsltproc.exe --output output/abstr.html ../prev/data/abstr.xsl ../tests/test.abstr.xml
+xsltproc.exe --output output/seman.html ../prev/data/seman.xsl ../tests/test.seman.xml
