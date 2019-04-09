cec464b12759b9802b2ffc470fe301a64ad02a7f
Žiga Vene <zv3896@student.uni-lj.si>
2019-04-09 21:39:05 +0200

    Frames: Imported template.

--- xsltproc/generate_xml.bat
+++ xsltproc/generate_xml.bat
@@ -2,3 +2,4 @@ xsltproc.exe --output output/lexan.html ../prev/data/lexan.xsl ../tests/test.lex
 xsltproc.exe --output output/synan.html ../prev/data/synan.xsl ../tests/test.synan.xml
 xsltproc.exe --output output/abstr.html ../prev/data/abstr.xsl ../tests/test.abstr.xml
 xsltproc.exe --output output/seman.html ../prev/data/seman.xsl ../tests/test.seman.xml
+xsltproc.exe --output output/frames.html ../prev/data/frames.xsl ../tests/test.frames.xml
