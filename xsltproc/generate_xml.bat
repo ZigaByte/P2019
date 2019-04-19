71a6c416eac30f40e840af26535ce92aea3198ef
ZigaVene <ziga.vene@gmail.com>
2019-04-20 00:06:13 +0200

    XML generator for this phase.

--- xsltproc/generate_xml.bat
+++ xsltproc/generate_xml.bat
@@ -3,3 +3,4 @@ xsltproc.exe --output output/synan.html ../prev/data/synan.xsl ../tests/test.syn
 xsltproc.exe --output output/abstr.html ../prev/data/abstr.xsl ../tests/test.abstr.xml
 xsltproc.exe --output output/seman.html ../prev/data/seman.xsl ../tests/test.seman.xml
 xsltproc.exe --output output/frames.html ../prev/data/frames.xsl ../tests/test.frames.xml
+xsltproc.exe --output output/imcgen.html ../prev/data/imcgen.xsl ../tests/test.imcgen.xml
