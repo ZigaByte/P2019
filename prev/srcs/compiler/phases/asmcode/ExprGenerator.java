03701327b31bbbac5e5211c20a6f94ad60bef3e0
ZigaVene <ziga.vene@gmail.com>
2019-05-05 17:18:38 +0200

    Some basic operations for AsmGen implemented.

--- prev/srcs/compiler/phases/asmcode/ExprGenerator.java
+++ prev/srcs/compiler/phases/asmcode/ExprGenerator.java
@@ -14,6 +14,64 @@ import compiler.data.asmcode.*;
  */
 public class ExprGenerator implements ImcVisitor<Temp, Vector<AsmInstr>> {
 
-    // TODO
+	@Override
+	public Temp visit(ImcCONST constant, Vector<AsmInstr> visArg) {
+		Vector<Temp> uses = new Vector<>();
+		Vector<Temp> defs = new Vector<>();
+		Vector<Label> jumps = new Vector<>();
+		
+		Temp temp = new Temp();
+		defs.add(temp);
+		
+		// TODO Transform constant into binary
+		visArg.add(new AsmOPER("SETL `d0, x="+constant.value, uses, defs, jumps));
+		visArg.add(new AsmOPER("INCML `d0, x="+constant.value, uses, defs, jumps));
+		visArg.add(new AsmOPER("INCMH `d0, x="+constant.value, uses, defs, jumps));
+		visArg.add(new AsmOPER("INCH `d0, x="+constant.value, uses, defs, jumps));
 
+		return temp;
+	}
+	
+	@Override
+	public Temp visit(ImcTEMP temp, Vector<AsmInstr> visArg) {
+		return temp.temp;
+	}
+	
+	@Override
+	public Temp visit(ImcBINOP binOp, Vector<AsmInstr> visArg) {
+		String instr = "";
+		switch (binOp.oper) {
+		case ADD:
+			instr = "ADD";
+			break;
+		}
+
+		Vector<Temp> defs = new Vector<>();
+		Vector<Temp> uses = new Vector<>();
+		Vector<Label> jumps = new Vector<>();
+		
+		Temp temp = new Temp();
+		defs.add(temp);
+		
+		uses.add(binOp.fstExpr.accept(this, visArg));
+		uses.add(binOp.sndExpr.accept(this, visArg));
+		
+		visArg.add(new AsmOPER(instr + " `d0, `s0, `s1", uses, defs, jumps));
+		
+		return temp;
+	}
+	
+	@Override
+	public Temp visit(ImcNAME name, Vector<AsmInstr> visArg) {
+		Vector<Temp> defs = new Vector<>();
+		Vector<Temp> uses = new Vector<>();
+		Vector<Label> jumps = new Vector<>();
+		
+		Temp temp = new Temp();
+		defs.add(temp);
+				
+		visArg.add(new AsmOPER("SET `d0, " + name.label.name, uses, defs, jumps));
+		
+		return temp;
+	}
 }
--- prev/srcs/compiler/phases/asmcode/StmtGenerator.java
+++ prev/srcs/compiler/phases/asmcode/StmtGenerator.java
@@ -15,6 +15,43 @@ import compiler.common.report.*;
  */
 public class StmtGenerator implements ImcVisitor<Vector<AsmInstr>, Object> {
 
-    // TODO
+	@Override
+	public Vector<AsmInstr> visit(ImcLABEL label, Object visArg) {
+		Vector<AsmInstr> toReturn = new Vector<AsmInstr>();
+		toReturn.add(new AsmLABEL(label.label));
+		return toReturn;
+	}
+	
+	@Override
+	public Vector<AsmInstr> visit(ImcMOVE move, Object visArg) {
+		Vector<AsmInstr> toReturn = new Vector<AsmInstr>();
 
+		Vector<Temp> defs = new Vector<>();
+		Vector<Temp> uses = new Vector<>();
+		
+		ImcExpr leftExpr = move.dst;
+		if(leftExpr instanceof ImcMEM) {
+			ImcMEM memExpr = (ImcMEM) move.dst;
+			
+			// Resolve address
+			uses.add(memExpr.addr.accept(new ExprGenerator(), toReturn));
+			// Resolve content
+			uses.add(move.src.accept(new ExprGenerator(), toReturn));
+			
+			toReturn.add(new AsmOPER("STO `s0, `s1, 0", uses, null, null));	
+		} else {
+			// Else move into temp			
+			defs.add(move.dst.accept(new ExprGenerator(), toReturn));
+			uses.add(move.src.accept(new ExprGenerator(), toReturn));
+			
+			toReturn.add(new AsmOPER("SET `d0, `s0", uses, defs, null));
+		}
+		
+		return toReturn;
+	}
+	
+	public Vector<AsmInstr> visit(ImcJUMP jump, Object visArg) {
+		return new Vector<AsmInstr>();
+	}
+	
 }
