b3b47edf21207981982f03851d76c9d4aa12e086
Žiga Vene <zv3896@student.uni-lj.si>
2019-05-14 11:40:22 +0200

    LiveAn template.

--- xsltproc/generate_xml.bat
+++ xsltproc/generate_xml.bat
@@ -6,3 +6,4 @@ xsltproc.exe --output output/frames.html ../prev/data/frames.xsl ../tests/test.f
 xsltproc.exe --output output/imcgen.html ../prev/data/imcgen.xsl ../tests/test.imcgen.xml
 xsltproc.exe --output output/chunks.html ../prev/data/chunks.xsl ../tests/test.chunks.xml
 xsltproc.exe --output output/asmgen.html ../prev/data/asmgen.xsl ../tests/test.asmgen.xml
+xsltproc.exe --output output/livean.html ../prev/data/livean.xsl ../tests/test.livean.xml
